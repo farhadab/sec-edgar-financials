cdb79c81c229b86414c1fcf7f0304c225176c11c
Farhad Abdolhosseini <farhadab15@gmail.com>
2019-04-19 23:43:29 -0400

    modified to replace repetition with iteration

--- edgar/document_text.py
+++ edgar/document_text.py
@@ -1,41 +1,34 @@
 from bs4 import BeautifulSoup
 from edgar.dtd import DTD
 
+
+# according to the EDGAR SGML specs, DOCUMENT.TEXT has the following children
+attrs = ['pdf', 'xml', 'xbrl', 'table', 'caption', 'stub', 'column', 'footnotes_section']
+
 class DocumentText:
+    '''
+    Used to model a DOCUMENT.TEXT element within an EDGAR SGML
+    '''
     dtd = DTD()
-    pdf = None
-    xml = None
-    xbrl = None
-    table = None
-    caption = None
-    stub = None
-    column = None
-    footnotes_section = None
 
     def __init__(self, data):
+        '''
+        Constructor
+
+        :param data: a dictionary of parsed SGML DOCUMENT.TEXT;
+            keys are tags and values are data as strings
+        '''
         self.data = data
 
-        if type(data) is dict and self.dtd.pdf.tag in data:
-            self.pdf = data[self.dtd.pdf.tag]
+        # use data to set attributes
+        for attr in attrs:
+            tag = getattr(self.dtd, attr).tag
 
-        if type(data) is dict and self.dtd.xml.tag in data:
-            xml_text = data[self.dtd.xml.tag]
-            self.xml = BeautifulSoup(xml_text, 'html.parser')
+            if type(data) is dict and tag in data:
+                value = data[tag]
 
-        if type(data) is dict and self.dtd.xbrl.tag in data:
-            self.xbrl = data[self.dtd.xbrl.tag]
+                if attr == 'xml':
+                    # for everything else, we take the text as is
+                    value = BeautifulSoup(value, 'html.parser')
 
-        if type(data) is dict and self.dtd.table.tag in data:
-            self.table = data[self.dtd.table.tag]
-
-        if type(data) is dict and self.dtd.caption.tag in data:
-            self.caption = data[self.dtd.caption.tag]
-
-        if type(data) is dict and self.dtd.stub.tag in data:
-            self.stub = data[self.dtd.stub.tag]
-
-        if type(data) is dict and self.dtd.column.tag in data:
-            self.column = data[self.dtd.column.tag]
-
-        if type(data) is dict and self.dtd.footnotes_section.tag in data:
-            self.footnotes_section = data[self.dtd.footnotes_section.tag]
+                setattr(self, attr, value)
